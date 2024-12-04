e033b4071a776436ee9ba27a9b6b00e67c378ed4
kdubaguntaepam <109850024+kdubaguntaepam@users.noreply.github.com>
2024-12-04 20:36:57 +0530

    Create API/StepDefinitions/PetStoreNegativeSteps.cs

---
+++ API/StepDefinitions/PetStoreNegativeSteps.cs
@@ -0,0 +1,44 @@
+using TechTalk.SpecFlow;
+using AutomationFramework.API.BusinessLogic;
+using AutomationFramework.Core.Config;
+using RestSharp;
+using FluentAssertions;
+using Serilog;
+
+namespace AutomationFramework.API.StepDefinitions
+{
+    [Binding]
+    public class PetStoreNegativeSteps
+    {
+        private readonly PetBusinessLogic _petBusinessLogic;
+        private RestResponse _response;
+        private ScenarioContext _scenarioContext;
+
+        public PetStoreNegativeSteps(ScenarioContext scenarioContext)
+        {
+            var baseUrl = ConfigManager.GetConfigValue<string>("ApiBaseUrl");
+            _scenarioContext = scenarioContext;
+            _petBusinessLogic = new PetBusinessLogic(baseUrl);
+        }
+
+        [When(@"I attempt to delete a pet with ID \"(.*)\"")]
+        public void WhenIAttemptToDeleteAPetWithID(string petId)
+        {
+            _response = _petBusinessLogic.DeletePet(petId);
+        }
+
+        [Then(@"the response status code should be (.*)")]
+        public void ThenTheResponseStatusCodeShouldBe(int expectedStatusCode)
+        {
+            _response.StatusCode.Should().Be((System.Net.HttpStatusCode)expectedStatusCode);
+            Log.Information($"Verified response status code: {_response.StatusCode}");
+        }
+
+        [Then(@"the response message should indicate that the pet was not found")]
+        public void ThenTheResponseMessageShouldIndicateThatThePetWasNotFound()
+        {
+            _response.Content.Should().Contain("Pet not found");
+            Log.Information("Verified response message indicates pet was not found.");
+        }
+    }
+}
