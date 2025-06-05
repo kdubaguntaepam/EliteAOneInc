6ee053a55302b89296a383fa1765d8ae8f3cc26d
kdubaguntaepam <109850024+kdubaguntaepam@users.noreply.github.com>
2025-06-05 11:51:35 +0530

    Update API/StepDefinitions/PetStoreNegativeSteps.cs

--- API/StepDefinitions/PetStoreNegativeSteps.cs
+++ API/StepDefinitions/PetStoreNegativeSteps.cs
@@ -13,6 +13,8 @@ namespace AutomationFramework.API.StepDefinitions
         private readonly PetBusinessLogic _petBusinessLogic;
         private RestResponse _response;
         private ScenarioContext _scenarioContext;
+        private const string InvalidPetIdKey = "InvalidPetID";
+
         public PetStoreNegativeSteps(ScenarioContext scenarioContext)
         {
             var baseUrl = ConfigManager.GetConfigValue<string>("ApiBaseUrl");
@@ -20,25 +22,33 @@ namespace AutomationFramework.API.StepDefinitions
             _petBusinessLogic = new PetBusinessLogic(baseUrl);
         }
 
-        [When(@"I retrieve a pet with an invalid ID")]
-        public void WhenIRetrieveAPetWithAnInvalidID()
+        [Given(@"I have an invalid pet ID")]
+        public void GivenIHaveAnInvalidPetID()
         {
-            var invalidPetId = -1; // Assuming -1 is an invalid ID
+            long invalidPetId = -1; // Assuming -1 is an invalid ID
+            ScenarioContextHelper.SetOrReplace(_scenarioContext, InvalidPetIdKey, invalidPetId);
+            Log.Information("Set an invalid pet ID for the test.");
+        }
+
+        [When(@"I retrieve the pet by invalid ID")]
+        public void WhenIRetrieveThePetByInvalidID()
+        {
+            var invalidPetId = _scenarioContext.Get<long>(InvalidPetIdKey);
             _response = _petBusinessLogic.GetPetById(invalidPetId);
         }
 
-        [Then(@"the response status code should be 404")]
-        public void ThenTheResponseStatusCodeShouldBe404()
+        [Then(@"the response status code should be 404 for invalid pet ID")]
+        public void ThenTheResponseStatusCodeShouldBe404ForInvalidPetID()
         {
             _response.StatusCode.Should().Be(System.Net.HttpStatusCode.NotFound);
-            Log.Information("Verified response status code: {StatusCode}", _response.StatusCode);
+            Log.Information("Verified response status code: 404 Not Found for invalid pet ID.");
         }
 
-        [Then(@"the response message should indicate that the pet was not found")]
-        public void ThenTheResponseMessageShouldIndicateThatThePetWasNotFound()
+        [Then(@"the error message should indicate pet not found")]
+        public void ThenTheErrorMessageShouldIndicatePetNotFound()
         {
-            _response.Content.Should().Contain("Pet not found");
-            Log.Information("Verified response message: {Content}", _response.Content);
+            _response.Content.Should().Contain("Pet not found", "The error message should indicate that the pet was not found.");
+            Log.Information("Verified error message indicates pet not found.");
         }
     }
 }
