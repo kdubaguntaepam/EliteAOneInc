25c0aebf08a87de8d35817905572a56c61b1e7a5
kdubaguntaepam <109850024+kdubaguntaepam@users.noreply.github.com>
2025-06-17 14:18:50 +0530

    Create API/StepDefinitions/PetStoreNegativeSteps.cs

---
+++ API/StepDefinitions/PetStoreNegativeSteps.cs
@@ -0,0 +1,37 @@
+using BoDi;
+using TechTalk.SpecFlow;
+using AutomationFramework.API.BusinessLogic;
+using AutomationFramework.Core.Config;
+using RestSharp;
+using FluentAssertions;
+using Serilog;
+
+namespace AutomationFramework.API.StepDefinitions
+{
+    [Binding]
+    public class PetStoreNegativeSteps
+    {
+        private readonly PetBusinessLogic _petBusinessLogic;
+        private RestResponse _response;
+        private ScenarioContext _scenarioContext;
+        public PetStoreNegativeSteps(ScenarioContext scenarioContext)
+        {
+            var baseUrl = ConfigManager.GetConfigValue<string>("ApiBaseUrl");
+            _scenarioContext = scenarioContext;
+            _petBusinessLogic = new PetBusinessLogic(baseUrl);
+        }
+
+        [When(@"I retrieve the pet by ID '(.*)'")]
+        public void WhenIRetrieveThePetByID(long petId)
+        {
+            _response = _petBusinessLogic.GetPetById(petId);
+        }
+
+        [Then(@"the response error message should be '(.*)'")]
+        public void ThenTheResponseErrorMessageShouldBe(string expectedErrorMessage)
+        {
+            _response.Content.Should().Contain(expectedErrorMessage);
+            Log.Information("Verified error message: {ErrorMessage}", expectedErrorMessage);
+        }
+    }
+}
