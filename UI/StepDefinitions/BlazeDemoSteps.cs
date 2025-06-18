95ed1e0ac182230267f7f5aafc1029754bc5238a
kdubaguntaepam <109850024+kdubaguntaepam@users.noreply.github.com>
2025-06-18 23:14:21 +0530

    Create UI/StepDefinitions/BlazeDemoSteps.cs

---
+++ UI/StepDefinitions/BlazeDemoSteps.cs
@@ -0,0 +1,32 @@
+using TechTalk.SpecFlow;
+using AutomationFramework.Core.Config;
+using OpenQA.Selenium;
+using FluentAssertions;
+
+namespace AutomationFramework.UI.StepDefinitions
+{
+    [Binding]
+    public class BlazeDemoSteps
+    {
+        private readonly IWebDriver _driver;
+
+        public BlazeDemoSteps(ScenarioContext scenarioContext)
+        {
+            _driver = (IWebDriver)scenarioContext["WebDriver"];
+        }
+
+        [Given(@"I navigate to BlazeDemo page")]
+        public void GivenINavigateToBlazeDemoPage()
+        {
+            var url = "https://blazedemo.com/";
+            _driver.Navigate().GoToUrl(url);
+        }
+
+        [Then(@"the page title should be '(.*)'")]
+        public void ThenThePageTitleShouldBe(string expectedTitle)
+        {
+            var actualTitle = _driver.Title;
+            actualTitle.Should().Be(expectedTitle, $"The page title should be '{expectedTitle}' but was '{actualTitle}'");
+        }
+    }
+}
